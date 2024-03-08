7d3ff3f74153b517b228f655171cad3011482b02
illmaticmd <mcdesmond.duru@werum.com>
2024-03-08 11:21:54 -0500

    latest python files

---
+++ test.py
@@ -0,0 +1,13 @@
+a1 = 17
+a2 = 34
+a3 = 8
+
+
+a1 >= 21 or a2 >= 21
+print(a1 >= 21 or a2 >= 21)
+print(a2 >= 21 or a1 >= 21)
+print(a3 >= 21 or a1 >= 21)
+
+x = 'perfume'
+mall_items = ['video game','doll','clothing','not perfume', 'perfume']
+print(x in mall_items)
